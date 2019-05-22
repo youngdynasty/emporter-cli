3007cb576f6fd7da5ffd5c83e65a1ccc76d2360f
Mike Pulaski <mikepulaski@gmail.com>
2019-05-22 18:17:57 +0200

    Bumped to version 0.1.1.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -20,7 +20,7 @@ set -e
 
 # Package code signatures are verified before installation
 PACKAGE_TEAM_ID=EEQTQC5N2L
-PACKAGE_VERSION=0.1.0
+PACKAGE_VERSION=0.1.1
 PACKAGE_URL=https://github.com/youngdynasty/emporter-cli/releases/download/v$PACKAGE_VERSION/emporter.pkg
 
 # Optional overrides
